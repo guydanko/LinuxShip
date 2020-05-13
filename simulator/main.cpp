ecc1bb001f85465b9590c736a1f7894ed64c0cc1
guydanko <guy.dankovich@gmail.com>
2020-05-13 17:16:14 +0300

    new main

--- simulator/main.cpp
+++ simulator/main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <iostream>
 #include "map"
+#include <fstream>
 
 namespace fs = std::filesystem;
 
@@ -17,34 +18,53 @@ int main(int argc, char *argv[]) {
             argAlgoPath = argc >= i + 2 ? argv[i + 1] : "";
         } else if (0 == outputFlag.compare(argv[i])) {
             argOutputPath = argc >= i + 2 ? argv[i + 1] : "";
-        } else{
+        } else {
             std::cerr << "Cannot run simulator, illegal flag - " << argv[i] << "!\n";
             return EXIT_FAILURE;
         }
-    }
-
-    if (argTravelPath.empty()) {
-        std::cerr << "Cannot run simulator, program must receive travel path!\n";
-        /*fatal error in file*/
-        return EXIT_FAILURE;
-    }
-    if (!fs::exists(argTravelPath)) {
-        std::cerr << "Cannot run simulator, travelPath does not exist!\n";
-        /*fatal error in file*/
-        return EXIT_FAILURE;
-    }
-    if (!argAlgoPath.empty() && !fs::exists(argAlgoPath)) {
-        std::cerr << "Cannot run simulator, algorithm path does not exist!\n";
-        /*fatal error in file*/
-        return EXIT_FAILURE;
     }
 
     const string travelPath = argTravelPath;
     const string outPath = argOutputPath.empty() ? fs::current_path().string() : argOutputPath;
     const string algoPath = argAlgoPath.empty() ? fs::current_path().string() : argAlgoPath;
 
+    string errorFilePath;
+
+    if (!fs::exists(outPath)) {
+        errorFilePath = fs::current_path().string() + "/simulator.errors";
+    } else {
+        errorFilePath = outPath + "/simulator.errors";
+    }
+
+
+    std::ofstream errorFile(errorFilePath);
+    bool toRunSimulator = true;
+
+    if (argTravelPath.empty()) {
+        errorFile << "Fatal error: program must receive travel path!\n";
+        toRunSimulator = false;
+    }
+    if (!fs::exists(argTravelPath)) {
+        errorFile << "Fatal error: travelPath does not exist!\n";
+        toRunSimulator = false;
+    }
+    if (!argAlgoPath.empty() && !fs::exists(argAlgoPath)) {
+        errorFile << "Fatal error: algorithm path does not exist!\n";
+        toRunSimulator = false;
+    }
+    if (!fs::exists(outPath)) {
+        errorFile << "Fatal error: output path does not exist!\n";
+        toRunSimulator = false;
+    }
+
+    if (!toRunSimulator) {
+        errorFile.close();
+        return EXIT_FAILURE;
+    }
+
     Simulator simulator(travelPath, algoPath, outPath);
     simulator.run();
+    errorFile.close();
     return EXIT_SUCCESS;
 
 }
